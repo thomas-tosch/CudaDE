168f4ee10310ffaa326a322a6a1fd1f1c1160d8c
thomas.tosch <thomas.tosch@uha.fr>
2021-11-24 14:10:19 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolution.cpp
+++ DifferentialEvolution.cpp
@@ -124,6 +124,7 @@ std::vector<float> DifferentialEvolution::fmin(void *args)
     differentialEvolution(d_target1, d_trial, d_cost, d_target2, d_min,
             d_max, h_cost, d_randStates, dim, popSize, numGenerations, CR, F, args,
             result.data());
+    std::cout << h_cost << std::endl;
     
     return result;
 }
