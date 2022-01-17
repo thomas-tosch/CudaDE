05f1ca0449d0ce4b72cf6053ff36efe4b6417792
thomas.tosch <thomas.tosch@uha.fr>
2022-01-17 15:28:00 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.cu
+++ DifferentialEvolutionGPU.cu
@@ -146,7 +146,7 @@ __device__ float rosenbrock(const float *vec, const void *args, const float *nex
 
     float sum = 0;
     for (int i = 0; i < a->dim - 1; i++) {
-        sum += 100 * pow(nextVec[i] - pow(vec[i], 2), 2) + pow(1 - vec[i], 2);
+        sum += 100 * pow(vec[i+1] - pow(vec[i], 2), 2) + pow(1 - vec[i], 2);
     }
     return sum + 390;
 }
