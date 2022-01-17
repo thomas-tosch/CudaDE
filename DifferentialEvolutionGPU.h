48ce7d6e10098d18c98dcaebe3131860cc74677a
thomas.tosch <thomas.tosch@uha.fr>
2022-01-17 15:57:19 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.h
+++ DifferentialEvolutionGPU.h
@@ -71,7 +71,7 @@
 #define SCHWEFEL 6
 
 
-#define COST_SELECTOR SCHWEFEL
+#define COST_SELECTOR ROSENBROCK
 
 // A basic macro used to checking cuda errors.
 // @param ans - the most recent enumerated cuda error to check.
