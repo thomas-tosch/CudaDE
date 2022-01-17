fbbd096647430d14548e9113140c52db4196f6c1
thomas.tosch <thomas.tosch@uha.fr>
2022-01-17 12:40:32 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.cu
+++ DifferentialEvolutionGPU.cu
@@ -411,7 +411,7 @@ void differentialEvolution(float *d_target,
     //std::cout << "Best cost = " << bestCost << " bestIdx = " << bestIdx << std::endl;
 
     // output best minimization
-    printCudaVector(d_target+(bestIdx*dim), dim);.
+    printCudaVector(d_target+(bestIdx*dim), dim);
     ret = cudaMemcpy(h_output, d_target+(bestIdx*dim), sizeof(float)*dim, cudaMemcpyDeviceToHost);
     gpuErrorCheck(ret);
 }
