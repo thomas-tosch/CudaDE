c12495daa111ec44be60d7f1e53e2ed540102f9e
thomas.tosch <thomas.tosch@uha.fr>
2022-01-18 15:21:18 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.cu
+++ DifferentialEvolutionGPU.cu
@@ -360,8 +360,8 @@ __global__ void evolutionKernel(float *d_target,
     float F4 = 0.2;
 
     float best = FLT_MAX;
-    int bestIdx = -1;
-    for (int i = 0; i < popSize * dim; i++) {
+    int bestIdx = 0;
+    for (int i = 0; i < popSize; i++) {
         if (d_cost[i] <= best) {
             best = d_cost[i];
             bestIdx = i;
