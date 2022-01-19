54473bb2b2543b58e8749015ab3943a8037860e6
thomas.tosch <thomas.tosch@uha.fr>
2022-01-19 15:10:34 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int runTest(int popSize, int dim, int costFun, float minBound, float maxBound, f
     // get the result from the minimizer
     float result = minimizer.fmin(d_x);
     std::cout << x.costFun << std::endl;
-    std::cout << "Result = " << result[0] << std::endl;
+    std::cout << "Result = " << result << std::endl;
     std::cout << "Finished main function." << std::endl;
     return 1;
 }
