e0417c5109f3cc02839295bd6d2396b689b03069
thomas.tosch <thomas.tosch@uha.fr>
2022-01-19 13:38:15 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.cu
+++ DifferentialEvolutionGPU.cu
@@ -266,16 +266,18 @@ __device__ float whitely(const float *vec, const void *args)
     return total;
 }
 
+__device__ float w(float x, float a, float b, float m)
+{
+    float sum = 0;
+    for (int k = 0; k <= m ; k++) {
+        sum += pow(a, k) * cos(2*M_PI*pow(b,k)*(x+0.5));
+    }
+    return sum;
+}
+
 __device__ float weierstrass(const float *vec, const void *args)
 {
     const struct data *a = (struct data *)args;
-    auto w = [](float x, float a, float b, float m) {
-        float sum = 0;
-        for (int k = 0; k <= m ; k++) {
-            sum += pow(a, k) * cos(2*M_PI*pow(b,k)*(x+0.5));
-        }
-        return sum;
-    };
     float sum = 0;
     for (int i = 0; i < a->dim; i++) {
         sum += w(vec[i], 0.5, 3, 20) - a->dim * w(0, 0.5, 3, 20);
