f490ef08971bfe1f9a73a0fc712e61ca6df813e7
thomas.tosch <thomas.tosch@uha.fr>
2022-01-20 11:08:05 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- DifferentialEvolutionGPU.cu
+++ DifferentialEvolutionGPU.cu
@@ -508,7 +508,7 @@ void differentialEvolution(float *d_target,
                            float *h_output)
 {
     cudaError_t ret;
-    int power32 = ceil(popSize / 8.0) * 8;
+    int power32 = ceil(popSize / 32.0) * 32;
     //std::cout << "power32 = " << power32 << std::endl;
 
     //std::cout << "min bounds = ";
