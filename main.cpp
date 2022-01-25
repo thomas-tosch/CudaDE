f8ac7ba528c2df7196bc3f8b993e70e3de4a8aef
thomas.tosch <thomas.tosch@uha.fr>
2022-01-25 15:17:47 +0100

    changes
    Signed-off-by: thomas.tosch <thomas.tosch@uha.fr>

--- main.cpp
+++ main.cpp
@@ -94,9 +94,9 @@ float runTestSequential(int popSize, int dim, int costFun, float minBound, float
 
     float d_target1[popSize * dim] = {};
     float d_trial[popSize * dim] = {};
-    float d_cost[popSize * xim] = {};
-    float d_target2[popSize * xim] = {};
-    float h_cost[popSize * xim] = {};
+    float d_cost[popSize * dim] = {};
+    float d_target2[popSize * dim] = {};
+    float h_cost[popSize * dim] = {};
     differentialEvolutionCPU(d_target1, d_trial, d_cost, d_target2, minBounds,
                              maxBounds, h_cost, dim, popSize, maxGen, cr, 0.5, x,
                              result.data());
